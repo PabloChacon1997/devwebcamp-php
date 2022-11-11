1c643bcc84c06da104457bdb2bb087b4ef724ec0
PabloChacon1997 <pabloandres120297@gmail.com>
2022-11-10 19:16:23 -0500

    Formulario Ponentes

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -11,4 +11,10 @@ class PonentesController {
       'titulo' => 'Ponentes / Conferencistas'
     ]);
   }
+  public static function crear(Router $router) {
+    $router->render('admin/ponentes/crear', [
+      'titulo' => 'Registrar Ponente',
+      'alertas' => [],
+    ]);
+  }
 }
