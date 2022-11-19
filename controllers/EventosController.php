4ce6b3fa51eba589ab68cc830743ea4826fcdee6
PabloChacon1997 <pabloandres120297@gmail.com>
2022-11-18 19:57:21 -0500

    Agregando funcionalidad el formulario eventos Creamos la tabla eventos y aÃadimos al formulario de registro de eventos las alertas, validamos los campos y trabajamos en js para capturar la categoria del evento y el dia para mostrar solo las fecha permitidas.

--- controllers/EventosController.php
+++ controllers/EventosController.php
@@ -6,6 +6,7 @@ use MVC\Router;
 
 use Model\Categoria;
 use Model\Dia;
+use Model\Evento;
 use Model\Hora;
 
 class EventosController {
@@ -20,11 +21,27 @@ class EventosController {
     $categorias = Categoria::all('ASC');
     $dias = Dia::all('ASC');
     $horas = Hora::all('ASC');
+    $evento = new Evento;
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $evento->sincronizar($_POST);
+
+      $alertas = $evento->validar();
+
+      if (empty($alertas)) {
+        $resultado = $evento->guardar();
+
+        if ($resultado) {
+          header('Location: /admin/eventos');
+        }
+      }
+    }
+
     $router->render('admin/eventos/crear', [
       'titulo' => 'Registrar Evento',
       'categorias' => $categorias,
       'dias' => $dias,
       'horas' => $horas,
+      'evento' => $evento,
       'alertas' => $alertas,
     ]);
   }
--- views/admin/eventos/formulario.php
+++ views/admin/eventos/formulario.php
@@ -8,6 +8,7 @@
       id="nombre"
       name="nombre"
       placeholder="Nombre evento"
+      value="<?php echo $evento->nombre; ?>"
     >
   </div>
   <div class="formulario__campo">
@@ -18,14 +19,15 @@
       id="descripcion"
       cols="30"
       rows="8"
-      placeholder="Descripción evento"></textarea>
+      placeholder="Descripción evento"
+      ><?php echo $evento->descripcion; ?></textarea>
   </div>
   <div class="formulario__campo">
     <label for="categoria" class="formulario__label">Categoría o Típo de Evento</label>
     <select class="formulario__select" name="categoria_id" id="categoria">
       <option selected disabled>- Seleccionar -</option>
       <?php foreach($categorias as $categoria) { ?>
-        <option value="<?php echo $categoria->id; ?>"><?php echo $categoria->nombre; ?></option>
+        <option <?php echo ($evento->categoria_id === $categoria->id) ? 'selected':''; ?> value="<?php echo $categoria->id; ?>"><?php echo $categoria->nombre; ?></option>
       <?php } ?>
     </select>
   </div>
@@ -43,11 +45,12 @@
         </div>
       <?php } ?>
     </div>
+    <input type="hidden" name="dia_id" value="">
   </div>
 
-  <div id="horas" class="formulario__campo">
+  <div  class="formulario__campo">
     <label for="" class="formulario__label">Seleccione la hora del Evento</label>
-    <ul class="horas">
+    <ul id="horas" class="horas">
         <?php foreach($horas as $hora) { ?>
           <li class="horas__hora"><?php echo $hora->hora; ?></li>
         <?php } ?>
@@ -75,6 +78,7 @@
       id="disponibles"
       name="disponibles"
       placeholder="Ejemplo: 20"
+      value="<?php echo $evento->disponibles; ?>"
     >
   </div>
 </fieldset>
