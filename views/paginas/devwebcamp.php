7db309e021e7f472370348f9ed38fec5fd4cac18
PabloChacon1997 <pabloandres120297@gmail.com>
2022-11-29 17:46:05 -0500

    Iniciando paginas publicas

---
+++ views/paginas/devwebcamp.php
@@ -0,0 +1,21 @@
+<div class="devwebcamp">
+  <h2 class="devwebcamp__heading"><?php echo $titulo; ?></h2>
+  <p class="devwebcamp__descripcion">Conoce la conferencia mas importante en Latinoamérica.</p>
+
+  <div class="devwebcamp__grid">
+    <div class="devwebcamp__imagen">
+      <picture>
+        <source srcset="build/img/sobre_devwebcamp.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/sobre_devwebcamp.jpg" alt="Imagen devwebcamp">
+      </picture>
+    </div>
+    <div class="devwebcamp__contenido">
+      <p class="devwebcamp__texto">
+      The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+      </p>
+      <p class="devwebcamp__texto">
+      The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+      </p>
+    </div>
+  </div>
+</div>
