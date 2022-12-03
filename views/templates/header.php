00c3f8985f8e71bb9a9938296bba57e1adf9fb5c
PabloChacon1997 <pabloandres120297@gmail.com>
2022-12-02 19:06:20 -0500

    Terminada area publica Finalizamos el area de inicio para todos los publicos, continuaremos con la pagina principal

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,8 +1,15 @@
 <header class="header">
   <div class="header__contenedor">
     <nav class="header__navegacion">
-      <a href="/registro" class="header__enlace">Registro</a>
-      <a href="/login" class="header__enlace">Iniciar sesion</a>
+      <?php if(is_auth()) { ?>
+        <a href="<?php echo is_admin() ? '/admin/dashboard':'/finalizar-registro' ?>" class="header__enlace">Administrar</a>
+        <form method="POST" action="/logout" class="header__form">
+          <input type="submit" value="Cerrar Sesión" class="header__submit--logout"/>
+        </form>
+      <?php } else { ?>
+        <a href="/registro" class="header__enlace">Registro</a>
+        <a href="/login" class="header__enlace">Iniciar sesion</a>
+      <?php } ?>
     </nav>
     <div class="header__contenido">
       <a href="/">
@@ -23,10 +30,10 @@
       </h2>
     </a>
     <nav class="navegacion">
-      <a href="/devwebcamp" class="navegacion__enlace">Eventos</a>
-      <a href="/paquetes" class="navegacion__enlace">Paquetes</a>
-      <a href="/workshop-conferencias" class="navegacion__enlace">Workshops / Conferencias</a>
-      <a href="/registro" class="navegacion__enlace">Comprar Pase</a>
+      <a href="/devwebcamp" class="navegacion__enlace <?php echo pagina_actual('/devwebcamp') ? 'navegacion__enlace--actual':''; ?>">Eventos</a>
+      <a href="/paquetes" class="navegacion__enlace <?php echo pagina_actual('/paquetes') ? 'navegacion__enlace--actual':''; ?>">Paquetes</a>
+      <a href="/workshop-conferencias" class="navegacion__enlace <?php echo pagina_actual('/workshop-conferencias') ? 'navegacion__enlace--actual':''; ?>">Workshops / Conferencias</a>
+      <a href="/registro" class="navegacion__enlace <?php echo pagina_actual('/registro') ? 'navegacion__enlace--actual':''; ?>">Comprar Pase</a>
     </nav>
   </div>
 </div>
