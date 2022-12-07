0832d274f7e84788e87d72f62741814cebf5cb1a
PabloChacon1997 <pabloandres120297@gmail.com>
2022-12-07 16:29:24 -0500

    Finalizado la pagina de inicio

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -89,4 +89,10 @@ class PaginasController {
       'eventos' => $eventos_formateados,
     ]);
   }
+
+  public static function error(Router $router) {
+    $router->render('paginas/error', [
+      'titulo' => 'Página no encontrada',
+    ]);
+  }
 }
--- includes/funciones.php
+++ includes/funciones.php
@@ -35,3 +35,11 @@ function is_admin(): bool {
     }
     return isset($_SESSION['admin']) && !empty($_SESSION['admin']);
 }
+
+function aos_animacion(): void {
+    $efectos = ['fade-up', 'fade-down', 'fade-left','fade-right','flip-left'
+    ,'flip-right','zoom-in', 'zoom-in-down','zoom-in-up', 'zoom-out'];
+    $efecto =  array_rand($efectos, 1);
+    echo 'data-aos="'.$efectos[$efecto].'"';
+    
+}
